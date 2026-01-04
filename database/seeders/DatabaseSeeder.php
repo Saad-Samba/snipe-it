255afaed005ed00aba72cd39af7f1fb7bba4d486
saadsamba <saad.samba.contact@gmail.com>
2026-01-04 15:59:26 +0100

    Added a seeder for the projects table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,6 +34,7 @@ class DatabaseSeeder extends Seeder
         $this->call(DepreciationSeeder::class);
         $this->call(ManufacturerSeeder::class);
         $this->call(SupplierSeeder::class);
+        $this->call(ProjectSeeder::class);
         $this->call(AssetModelSeeder::class);
         $this->call(DepreciationSeeder::class);
         $this->call(StatuslabelSeeder::class);
