c43b5efd1d789600cda4243c434dfeabb54e904d
Saad-Samba <89049334+Saad-Samba@users.noreply.github.com>
2026-01-22 17:47:24 +0100

    Tidy transfer modal spacing and help text

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -1199,7 +1199,8 @@
                     'fieldname' => 'transfer_location_id',
                     'translated_name' => trans('admin/hardware/form.default_location'),
                     'help_text' => trans('admin/users/general.transfer_default_location_help'),
-                    'hide_new' => 'true'
+                    'hide_new' => 'true',
+                    'style' => 'margin-top: 15px;'
                   ])
                   <div class="row">
                       <div class="col-md-7 col-md-offset-3">
