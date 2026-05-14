1634606ba8bf47fb76dbe3e838111e2cc4b80510
saadsamba <saad.samba.contact@gmail.com>
2026-05-14 21:12:33 +0100

    Simplify RAC request email table

--- resources/views/notifications/markdown/rac-request-summary.blade.php
+++ resources/views/notifications/markdown/rac-request-summary.blade.php
@@ -12,16 +12,12 @@
 **{{ trans('general.requested') }}:** {{ $submitted_at }}
 
 @component('mail::table')
-| Model | {{ trans('general.project') }} | {{ trans('general.discipline') }} | {{ trans('general.qty') }} | {{ trans('mail.rac_reusable_now_in_scope') }} | Needed By |
-| ------------- | ------------- | ------------- | ------------- | ------------- | ------------- |
+| Model | {{ trans('general.qty') }} | Needed By |
+| ------------- | ------------- | ------------- |
 @foreach($lines as $line)
-| [{{ $line['model_name'] }}]({{ $line['model_show_url'] }}) | {{ $line['project_name'] }} | {{ $line['discipline_name'] }} | {{ $line['requested_quantity'] }} | {{ $line['reusable_quantity'] }} | {{ $line['needed_by_date'] }} |
+| [{{ $line['model_name'] }}]({{ $line['model_show_url'] }}) | {{ $line['requested_quantity'] }} | {{ $line['needed_by_date'] }} |
 @endforeach
 @endcomponent
-
-@foreach($lines as $line)
-- [{{ trans('mail.rac_project_link') }}]({{ $line['project_requests_url'] }}) / [{{ trans('mail.rac_review_link') }}]({{ $line['request_detail_url'] }})
-@endforeach
 
 {{ trans('mail.best_regards') }}
 
