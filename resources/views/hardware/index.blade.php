46eee4ce99adfaa273365944736ce03fbeb83bf5
saadsamba <saad.samba.contact@gmail.com>
2026-08-10 18:34:07 +0100

    Show assets table filter controls

--- resources/views/hardware/index.blade.php
+++ resources/views/hardware/index.blade.php
@@ -146,6 +146,7 @@
                 data-sort-name="name"
                 data-search-text="{{ Request::filled('request_id') ? '' : session()->get('search') }}"
                 data-filter-control="true"
+                data-filter-control-visible="true"
                 data-show-columns-search="true"
                 data-toolbar="#assetsBulkEditToolbar"
                 data-bulk-button-id="#bulkAssetEditButton"
