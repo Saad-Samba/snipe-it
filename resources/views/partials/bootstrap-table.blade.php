a9b09b4b6dc5625136f44ef63cd8b25b0b190b1c
saadsamba <saad.samba.contact@gmail.com>
2026-08-10 18:41:06 +0100

    Load bootstrap table filter-control assets

--- resources/views/partials/bootstrap-table.blade.php
+++ resources/views/partials/bootstrap-table.blade.php
@@ -1,10 +1,12 @@
 @push('css')
     <link rel="stylesheet" href="{{ url(mix('css/dist/bootstrap-table.css')) }}">
+    <link rel="stylesheet" href="{{ url('css/dist/bootstrap-table-filter-control.css') }}">
 @endpush
 
 @push('js')
 
 <script src="{{ url(mix('js/dist/bootstrap-table.js')) }}"></script>
+<script src="{{ url('js/dist/bootstrap-table-filter-control.js') }}"></script>
 <script src="{{ url(mix('js/dist/bootstrap-table-locale-all.min.js')) }}"></script>
 
 <!-- load english again here, even though it's in the all.js file, because if BS table doesn't have the translation, it otherwise defaults to chinese. See https://bootstrap-table.com/docs/api/table-options/#locale -->
